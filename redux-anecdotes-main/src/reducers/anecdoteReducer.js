2d10bb8300eaae157504d9813d608867f9287a1e
nartkosova <nartkosova21@gmail.com>
2024-09-25 00:08:21 +0300

    exercise 6.3 to 6.8

--- redux-anecdotes-main/src/reducers/anecdoteReducer.js
+++ redux-anecdotes-main/src/reducers/anecdoteReducer.js
@@ -1,7 +1,7 @@
 const anecdotesAtStart = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time... The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
@@ -23,7 +23,32 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch (action.type) {
+    case 'VOTE':
+      return state.map(anecdote =>
+        anecdote.id === action.payload.id
+          ? { ...anecdote, votes: anecdote.votes + 1 }
+          : anecdote
+      )
+    case 'CREATE_ANECDOTE':
+      const newAnecdote = asObject(action.payload.content)
+      return [...state, newAnecdote]
+    default:
+      return state
+  }
 }
+export const vote = (id) => {
+  return{
+    type: 'VOTE',
+    payload: { id },
+  }
+}
+export const createAnecdote = (content) => {
+  return{
+    type: 'CREATE_ANECDOTE',
+    payload: { content },
+  }
+}
+
 
 export default reducer
