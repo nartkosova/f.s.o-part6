9b5c459f41f8bca41ca6dfd6079cd37db7f40bc0
nartkosova <nartkosova21@gmail.com>
2024-09-25 18:37:02 +0300

    exercise 6.9

--- redux-anecdotes-main/src/App.jsx
+++ redux-anecdotes-main/src/App.jsx
@@ -1,10 +1,12 @@
 import AddAnecdote from './components/anecdoteForm'
 import AnecdoteList from './components/anecdoteList'
+import Filter from './components/Filter'
 
 const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Filter />
       <AnecdoteList />
       <AddAnecdote />
     </div>
--- redux-anecdotes-main/src/components/anecdoteList.jsx
+++ redux-anecdotes-main/src/components/anecdoteList.jsx
@@ -2,8 +2,16 @@ import { useSelector, useDispatch } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
+
+  const anecdotes = useSelector(({ anecdotes, filter }) => {
+    return anecdotes
+      .filter(anecdote =>
+        anecdote.content.toLowerCase().includes(filter.toLowerCase())
+      )
+      .slice()
+      .sort((a, b) => b.votes - a.votes)
+  })
 
   const addVote = (id) => {
     dispatch(vote(id))
@@ -11,20 +19,15 @@ const AnecdoteList = () => {
 
   return (
     <div>
-      {anecdotes
-        .slice() 
-        .sort((a, b) => b.votes - a.votes) 
-        .map(anecdote => (
-          <div key={anecdote.id}>
-            <div>
-              {anecdote.content}
-            </div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => addVote(anecdote.id)}>vote</button>
-            </div>
+      {anecdotes.map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => addVote(anecdote.id)}>vote</button>
           </div>
-        ))}
+        </div>
+      ))}
     </div>
   )
 }
